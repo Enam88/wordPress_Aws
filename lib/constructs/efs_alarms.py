3c0d9e8deb515d9f8c0392036a997c60bac4ce05
Enam AKLI <akli.ena88@gmail.com>
2024-02-06 15:19:31 +0100

    efs_alarms update

--- lib/constructs/efs_alarms.py
+++ lib/constructs/efs_alarms.py
@@ -2,12 +2,10 @@ from aws_cdk import aws_cloudwatch as cloudwatch
 from aws_cdk import aws_sns as sns
 from aws_cdk import aws_sns_subscriptions as subscriptions
 from aws_cdk import aws_efs as efs
-from aws_cdk import Stack
 from constructs import Construct
 
 class EfsAlarmsConstruct(Construct):
-
-    def __init__(self, scope: Construct, id: str, file_system: efs.FileSystem, email_address: str):
+    def __init__(self, scope: Construct, id: str, file_system: efs.IFileSystem, email_address: str):
         super().__init__(scope, id)
 
         # SNS topic for alarm notifications
@@ -18,11 +16,20 @@ class EfsAlarmsConstruct(Construct):
         # Subscribe the provided email address to the topic
         topic.add_subscription(subscriptions.EmailSubscription(email_address))
 
+        # Define the metric for BurstCreditBalance
+        burst_credit_balance_metric = cloudwatch.Metric(
+            namespace='AWS/EFS',
+            metric_name='BurstCreditBalance',
+            dimensions_map={
+                'FileSystemId': file_system.file_system_id
+            }
+        )
+
         # CloudWatch alarm for EFS BurstCreditBalance - Warning
         cloudwatch.Alarm(self, "EfsBurstCreditBalanceWarning",
                          alarm_name=f"EFS BurstCreditBalance Warning",
                          alarm_description="Warning if the BurstCreditBalance is too low",
-                         metric=file_system.metric("BurstCreditBalance"),
+                         metric=burst_credit_balance_metric,
                          threshold=180 * 60 * 60,  # Adjust based on your needs
                          evaluation_periods=1,
                          comparison_operator=cloudwatch.ComparisonOperator.LESS_THAN_THRESHOLD)
@@ -31,8 +38,7 @@ class EfsAlarmsConstruct(Construct):
         cloudwatch.Alarm(self, "EfsBurstCreditBalanceCritical",
                          alarm_name=f"EFS BurstCreditBalance Critical",
                          alarm_description="Critical alarm if the BurstCreditBalance is too low",
-                         metric=file_system.metric("BurstCreditBalance"),
+                         metric=burst_credit_balance_metric,
                          threshold=60 * 60 * 60,  # Adjust based on your needs
                          evaluation_periods=1,
                          comparison_operator=cloudwatch.ComparisonOperator.LESS_THAN_THRESHOLD)
-
