dd52322a0b42a749986082dde70bd41f7c6f32d5
Enam AKLI <akli.ena88@gmail.com>
2024-02-07 11:39:21 +0100

    alb update with target

--- lib/constructs/alb.py
+++ lib/constructs/alb.py
@@ -1,14 +1,15 @@
-from aws_cdk import aws_elasticloadbalancingv2 as elbv2
-from aws_cdk import aws_ec2 as ec2
-from aws_cdk import aws_certificatemanager as acm
+from aws_cdk import (
+    aws_elasticloadbalancingv2 as elbv2,
+    aws_ec2 as ec2,
+    aws_certificatemanager as acm
+)
 from constructs import Construct
-from aws_cdk import Stack
 import aws_cdk as cdk
 
 class ALBConstruct(Construct):
 
-    def __init__(self, scope: Construct, id: str, vpc: ec2.Vpc, public_alb_sg: ec2.SecurityGroup, ssl_certificate_arn: str = None):
-        super().__init__(scope, id)
+    def __init__(self, scope: Construct, id: str, vpc: ec2.Vpc, public_alb_sg: ec2.SecurityGroup, ssl_certificate_arn: str = None, **kwargs):
+        super().__init__(scope, id, **kwargs)
 
         # Create an internet-facing ALB
         self.lb = elbv2.ApplicationLoadBalancer(
@@ -20,8 +21,8 @@ class ALBConstruct(Construct):
             vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC)
         )
 
-        # Create a target group
-        self.target_group = elbv2.ApplicationTargetGroup(
+        # Create a target group for HTTP traffic
+        self.alb_target_group = elbv2.ApplicationTargetGroup(
             self, "PublicAlbTargetGroup",
             vpc=vpc,
             port=80,
@@ -32,14 +33,16 @@ class ALBConstruct(Construct):
                 timeout=cdk.Duration.seconds(5),
                 unhealthy_threshold_count=5
             ),
-            stickiness_cookie_duration=cdk.Duration.minutes(5)
+            stickiness_cookie_duration=cdk.Duration.minutes(5),
+            target_type=elbv2.TargetType.INSTANCE
         )
 
-        # Create an HTTP listener and optionally an HTTPS listener if an SSL certificate ARN is provided
-        self.http_listener = self.lb.add_listener("HttpListener", port=80, default_action=elbv2.ListenerAction.forward([self.target_group]))
+        # Create an HTTP listener
+        self.http_listener = self.lb.add_listener("HttpListener", port=80, default_action=elbv2.ListenerAction.forward([self.alb_target_group]))
 
+        # Optionally create an HTTPS listener if an SSL certificate ARN is provided
         if ssl_certificate_arn:
+            certificate = acm.Certificate.from_certificate_arn(self, "Certificate", ssl_certificate_arn)
             self.https_listener = self.lb.add_listener("HttpsListener", port=443,
-                                                        certificates=[elbv2.ListenerCertificate(ssl_certificate_arn)], 
-                                                        default_action=elbv2.ListenerAction.forward([self.target_group]))
-
+                                                        certificates=[certificate], 
+                                                        default_action=elbv2.ListenerAction.forward([self.alb_target_group]))
